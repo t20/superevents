7a7a73df60867fe7db494fe44166f9d5136b95e3
t20 <barbi.bruce@gmail.com>
2014-11-22 21:59:11 -0800

    Remove js alerts

--- js/superevents_admin.js
+++ js/superevents_admin.js
@@ -1,6 +1,4 @@
-alert ('This is a event admin page');
 var $j = jQuery.noConflict();
 $j(document).ready(function(){
-    alert ('This is a event admin page. Doc is now ready');
    $j('.datepicker').datepicker();
 });
